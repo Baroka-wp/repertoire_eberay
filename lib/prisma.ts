5e1b9c26b18b909ebd1e0d6acbc0be6146a11874
IROTORI Baroka <baroka@globodai.com>
2025-12-03 16:24:15 +0100

    fix: build errror for vercel

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,20 @@
 // lib/prisma.ts
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+declare global {
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient
+}
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+let prisma: PrismaClient
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+  prisma = global.prisma
+}
+
+export { prisma }
